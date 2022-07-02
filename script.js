91cde79b8555b1c9e16984bf306bf16ab40f81c2
getlucky13 <100950736+getlucky13@users.noreply.github.com>
2022-07-02 13:23:01 -0400

    Add vars, selectors, and some functions
    Adds necessary vars and element selectors, as well as four of the necessary functions for operations

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldClearDisplay = false;
+
+const numBtns = document.querySelectorAll('button.numbBtn');
+const opBtns = document.querySelectorAll('button.opBtns');
+const equalsBtn = document.querySelector('button.equalBtn');
+const clrBtn = document.querySelector('button.clrBtn')
+const dltBtn = document.querySelector('buttn.dltBtn');
+const displayCurrent = document.querySelector('div.displayCurrent');
+const displayPrevious = document.querySelectory('div.displayPrevious');
+
+
+
+function appendDigit(number) {
+  if(currentDisplay.textContent === '0' || shouldClearDisplay) {clearCurrentDisplay();}
+  currentDisplay.textContent += number;
+}
+
+function appendPeriod() {
+  if(shouldClearDisplay) {clearCurrentDisplay()}
+  if(currentDisplay.textContent === '') {currentDisplay.textContent = '0';}
+  if(currentDisplay.textContnet.includes('.'))return;
+  currentDisplay.textContent += '.';
+}
+
+function clearCurrentDispla() {
+  currentDisplay.textContent = '';
+  shouldClearDisplay = false;
+}
+
+function setOperation(operator) {
+  if(currentOperation !== null) evaluate();
+  firstOperand = currentDisplay.textContent;
+  currentOperation = operator;
+  previousDisplay.textContent = `${firstOperand} ${currentOperation}`;
+  shouldClearScreen = true;
+}
+
 function add(a, b) {
   return a + b;
 }
